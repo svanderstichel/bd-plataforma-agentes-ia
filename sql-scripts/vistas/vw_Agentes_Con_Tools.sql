078315741db3a5bbbb3f1b02720698d1994aabb6
svanderstichel <vanderstichel@gmail.com>
2025-10-25 13:36:54 -0300

    se añadio valor default a la fecha de creación de los agentes y comento el codigo de creacion de la vw_agente_con_tools

--- sql-scripts/vistas/vw_Agentes_Con_Tools.sql
+++ sql-scripts/vistas/vw_Agentes_Con_Tools.sql
@@ -1,4 +1,4 @@
--- Se debe desarrollar una vista (`VIEW`) en la base de datos con el nombre `vw_Agentes_Con_Tools`. La vista debe consolidar la información de las tablas `Agente`, `AgenteTool` y `Tool` para presentar un listado claro de los agentes y sus respectivas herramientas. Adicionalmente, Se deben incluir en el resultado únicamente aquellos agentes cuyo campo `Activo` en la tabla `Agente`.
+-- La vista vw_Agentes_Con_Tools consolida la información de las tablas `Agente`, `AgenteTool` y `Tool` para presentar un listado claro de los agentes y sus respectivas herramientas. Adicionalmente, solo se incluye en el resultado aquellos agentes cuyo campo es `Activo` en la tabla `Agente`.
 USE SistemaIA;
 GO
 
@@ -9,4 +9,3 @@ AS
         INNER JOIN AgenteTool AT ON A.IdAgente = AT.IdAgente
         INNER JOIN Tool T ON T.IdTool = AT.IdTool
     WHERE A.Activo = 1;
-
