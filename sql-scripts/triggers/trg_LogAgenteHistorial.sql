8beae6a851d3788d7d04deb41fb076cc67813b69
Mauro Sebastian Arias <maurosebastianarias@gmail.com>
2025-11-01 20:08:18 -0300

    Creacion de trg_LogAgenteHistorial sin implementacion

---
+++ sql-scripts/triggers/trg_LogAgenteHistorial.sql
@@ -0,0 +1,18 @@
+-- Se debe desarrollar un trigger (TRIGGER) para la tabla Agente que se active después de una actualización (UPDATE). El trigger deberá registrar el valor anterior de la instrucción en la tabla AgenteHistorial.
+
+USE SistemaIA;
+GO
+CREATE TRIGGER trg_LogAgenteHistorial ON Agente AFTER UPDATE
+AS 
+BEGIN
+
+    IF UPDATE(Instruccion) 
+    BEGIN
+        INSERT INTO AgenteHistorial
+            (IdAgente,Instruccion,Fecha)
+        SELECT IdAgente, Instruccion, GETDATE()
+        FROM deleted;
+    END
+END;    
+
+
